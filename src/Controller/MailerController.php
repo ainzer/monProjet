9526a16611685297fcd1b75e6d98673522d65644
Tony <127862337+ainzer@users.noreply.github.com>
2024-02-29 16:41:29 +0100

    29/02/2024

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MailerController extends AbstractController
+{
+    
+    #[Route('/email', name: 'app_mailer')]
+    public function sendEmail(MailerInterface $mailer): Response
+    {
+        try {
+            
+            $email = (new TemplatedEmail())
+                ->from('tututoto@gmail.com')
+                ->to('test@example.com')
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->htmlTemplate('mailer/index.html.twig'); 
+
+            ;
+            $mailer->send($email);
+
+            return new Response('Email sent successfully');
+        } catch (\Exception $e) {
+            return new Response('Error sending email: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
