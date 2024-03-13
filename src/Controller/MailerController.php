4b846d6b661c14c1d0da6382e8cd0a12b2ae45ae
Tony <127862337+ainzer@users.noreply.github.com>
2024-03-13 13:25:57 +0100

    13/03/2024

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -2,38 +2,59 @@
 
 namespace App\Controller;
 
-use SebastianBergmann\Template\Template;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\Mime\Email;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Mime\Part\DataPart;
 use Symfony\Component\Mime\Part\File;
 
-
 class MailerController extends AbstractController
 {
-    
-    #[Route('/email', name: 'app_mailer')]
+    #[Route('/mailer', name: 'app_mailer')]
     public function sendEmail(MailerInterface $mailer): Response
     {
-        try {
-            
-            $email = (new TemplatedEmail())
-                ->from('tututoto@gmail.com')
-                ->to('test@example.com')
-                ->subject('Time for Symfony Mailer!')
-                ->text('Sending emails is fun again!')
-                ->htmlTemplate('mailer/index.html.twig'); 
+        $email = (new TemplatedEmail())
+            ->from('hello@example.com')
+            //->to('you@example.com')
+            ->to(new Address('ryan@example.com'))
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            //->text('Sending emails is fun again!')
 
-            ;
-            $mailer->send($email);
 
-            return new Response('Email sent successfully');
-        } catch (\Exception $e) {
-            return new Response('Error sending email: ' . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
-        }
+            ->htmlTemplate('emails/signup.html.twig')
+
+            ->context([
+                'expiration_date' => new \DateTime('+7 days'),
+                'username' => 'foo',
+            ])
+
+            ->addPart(new DataPart(new File('/path/to/documents/terms-of-use.pdf')))
+            // vous pouvez, si vous le souhaitez, demander aux clients mail d'afficher un certain nom pour le fichier 
+            ->addPart(new DataPart(new File('/path/to/documents/privacy.pdf'), 'Privacy Policy'))
+            // vous pouvez aussi spécifier le type de document (autrement, il est deviné)
+            ->addPart(new DataPart(new File('/path/to/documents/contract.doc'), 'Contract', 'application/msword'))
+
+            ->addPart((new DataPart(fopen('/path/to/images/logo.png', 'r'), 'logo', 'image/png'))->asInline())
+            ->addPart((new DataPart(new File('/path/to/images/signature.gif'), 'footer-signature', 'image/gif'))->asInline())
+
+            // utiliser la syntaxe 'cid:' + "nom de l'image intégrée " pour référencer l'image
+            ->html('<img src="cid:logo"> ... <img src="cid:footer-signature"> ...')
+
+            // utiliser la même syntaxe pour les images intégrées en tant que background
+            ->html('... <div background="cid:footer-signature"> ... </div> ...');
+
+        $mailer->send($email);
+
+        return $this->render('mailer/index.html.twig', [
+            'controller_name' => 'MailerController',
+        ]);
     }
 }
