d964d94f687481bb8ee737e1d443e9887fb3e2e1
Tony <127862337+ainzer@users.noreply.github.com>
2024-04-04 10:04:58 +0200

    04/04/2024

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -24,7 +24,7 @@ class AccueilController extends AbstractController
 
     }
 
-    #[Route('/accueil', name: 'app_accueil')]
+    #[Route('/', name: 'app_accueil')]
     public function index(): Response
     {
 
