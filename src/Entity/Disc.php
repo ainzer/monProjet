46f57dc7fd4f15d1ddfb15f9a116c4d3fc48d31e
Tony <127862337+ainzer@users.noreply.github.com>
2024-04-04 10:26:54 +0200

    04/04/2024

--- src/Entity/Disc.php
+++ src/Entity/Disc.php
@@ -2,10 +2,16 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\DiscRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: DiscRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['read']],
+    denormalizationContext: ['groups' => ['write']],
+)]
 class Disc
 {
     #[ORM\Id]
@@ -14,15 +20,19 @@ class Disc
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['read'])]
     private ?string $title = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['read', 'write'])]
     private ?string $picture = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
+    #[Groups(['read'])]
     private ?string $label = null;
 
     #[ORM\ManyToOne(inversedBy: 'discs')]
+    #[Groups(['read'])]
     private ?Artist $artist = null;
 
     public function getId(): ?int
